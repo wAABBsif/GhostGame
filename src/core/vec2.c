3a30adfc35060ba887ea7d0b641be01544f27d95
WasifBoomz <wasifultragamer@gmail.com>
2025-09-09 17:36:30 -0400

    Implement vec2

---
+++ src/core/vec2.c
@@ -0,0 +1,116 @@
+﻿#include "vec2.h"
+#include <math.h>
+
+const vec2 VEC2_ZERO = {0.0f, 0.0f};
+const vec2 VEC2_ONE = {1.0f, 1.0f};
+const vec2 VEC2_UNIT_X = {1.0f, 0.0f};
+const vec2 VEC2_UNIT_Y = {0.0f, 1.0f};
+
+vec2 vec2_from_angle(const float angle)
+{
+	return (vec2){cos(angle), sin(angle)};
+}
+
+vec2 vec2_neg(const vec2 v)
+{
+	return (vec2){-v.x, -v.y};
+}
+
+vec2 vec2_add(const vec2 a, const vec2 b)
+{
+	return (vec2){a.x + b.x, a.y + b.y};
+}
+
+vec2 vec2_sub(const vec2 a, const vec2 b)
+{
+	return (vec2){a.x - b.x, a.y - b.y};
+}
+
+vec2 vec2_mul(const vec2 v, const float scalar)
+{
+	return (vec2){v.x * scalar, v.y * scalar};
+}
+
+vec2 vec2_div(const vec2 v, const float scalar)
+{
+	return (vec2){v.x / scalar, v.y / scalar};
+}
+
+vec2 vec2_scale(const vec2 a, const vec2 b)
+{
+	return (vec2){a.x * b.x, a.y * b.y};
+}
+
+float vec2_sqr_mag(const vec2 v)
+{
+	return v.x * v.x + v.y * v.y;
+}
+
+float vec2_mag(const vec2 v)
+{
+	return sqrtf(vec2_sqr_mag(v));
+}
+
+vec2 vec2_set_mag(const vec2 v, const float magnitude)
+{
+	return vec2_mul(vec2_normalize(v), magnitude);
+}
+
+vec2 vec2_clamp_mag(const vec2 v, const float max_magnitude)
+{
+	return vec2_mul(vec2_normalize(v), fmaxf(max_magnitude, vec2_mag(v)));
+}
+
+vec2 vec2_normalize(const vec2 v)
+{
+	return vec2_div(v, vec2_mag(v));
+}
+
+float vec2_angle(const vec2 v)
+{
+	return atan2f(v.y, v.x);
+}
+
+float vec2_dot(const vec2 a, const vec2 b)
+{
+	return a.x * b.x + a.y * b.y;
+}
+
+vec2 vec2_rotate(const vec2 v, float angle)
+{
+	angle += vec2_angle(v);
+	return vec2_mul(vec2_from_angle(angle), vec2_mag(v));
+}
+
+vec2 vec2_project(const vec2 a, const vec2 b)
+{
+	float mag = vec2_dot(a, b);
+	mag /= vec2_sqr_mag(b);
+	return vec2_mul(b, mag);
+}
+
+vec2 vec2_project_on_plane(const vec2 a, vec2 normal)
+{
+	normal = vec2_normalize(normal);
+	return vec2_sub(a, vec2_project(a, normal));
+}
+
+float vec2_angle_to(const vec2 a, const vec2 b)
+{
+	return vec2_angle(a) - vec2_angle(b);
+}
+
+bool vec2_equal(const vec2 a, const vec2 b)
+{
+	return a.x == b.x && a.y == b.y;
+}
+
+bool vec2_is_zero(const vec2 v)
+{
+	return v.x == 0 && v.y == 0;
+}
+
+bool vec2_is_normalized(const vec2 v)
+{
+	return vec2_sqr_mag(v) == 1.0f;
+}
---
+++ src/core/vec2.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <stdbool.h>
+
+typedef struct vec2
+{
+	float x;
+	float y;
+} vec2;
+
+extern const vec2 VEC2_ZERO;
+extern const vec2 VEC2_ONE;
+extern const vec2 VEC2_UNIT_X;
+extern const vec2 VEC2_UNIT_Y;
+
+vec2 vec2_from_angle(float angle);
+
+vec2 vec2_neg(vec2 v);
+vec2 vec2_add(vec2 a, vec2 b);
+vec2 vec2_sub(vec2 a, vec2 b);
+vec2 vec2_mul(vec2 v, float scalar);
+vec2 vec2_div(vec2 v, float scalar);
+vec2 vec2_scale(vec2 a, vec2 b);
+
+float vec2_sqr_mag(vec2 v);
+float vec2_mag(vec2 v);
+vec2 vec2_set_mag(vec2 v, float magnitude);
+vec2 vec2_clamp_mag(vec2 v, float max_magnitude);
+vec2 vec2_normalize(vec2 v);
+
+float vec2_angle(vec2 v);
+float vec2_dot(vec2 a, vec2 b);
+vec2 vec2_rotate(vec2 v, float angle);
+vec2 vec2_project(vec2 a, vec2 b);
+vec2 vec2_project_on_plane(vec2 a, vec2 normal);
+
+bool vec2_equal(vec2 a, vec2 b);
+bool vec2_is_zero(vec2 v);
+bool vec2_is_normalized(vec2 v);
