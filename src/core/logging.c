fa52cb14afce77e732c7050ec8b842dfb1d4f8fa
WasifBoomz <wasifultragamer@gmail.com>
2025-10-04 19:47:48 -0400

    Implement Windows console colors

--- src/core/logging.c
+++ src/core/logging.c
@@ -3,6 +3,32 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#if defined(WIN32)
+#include <windows.h>
+#define SET_CONSOLE_TEXT_COLOR_WHITE() \
+	{	\
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);	\
+		SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	\
+	}
+#define SET_CONSOLE_TEXT_COLOR_YELLOW()	\
+	{	\
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);	\
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN);	\
+	}
+#define SET_CONSOLE_TEXT_COLOR_RED() \
+	{	\
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);	\
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);	\
+	}
+#endif
+
+#ifndef SET_CONSOLE_TEXT_COLOR_WHITE
+#define SET_CONSOLE_TEXT_COLOR_WHITE()
+#define SET_CONSOLE_TEXT_COLOR_YELLOW()
+#define SET_CONSOLE_TEXT_COLOR_RED()
+#endif
+
+
 void _log_vraw(const char *format, const va_list args)
 {
 	vprintf(format, args);
@@ -30,9 +56,11 @@ void log_warning(const char *format, ...)
 	va_list list;
 	va_start(list, format);
 
+	SET_CONSOLE_TEXT_COLOR_YELLOW();
 	_log_raw("[WARNING] ");
 	_log_vraw(format, list);
 	_log_raw("\n");
+	SET_CONSOLE_TEXT_COLOR_WHITE();
 }
 
 void log_error(const char *format, ...)
@@ -40,7 +68,9 @@ void log_error(const char *format, ...)
 	va_list list;
 	va_start(list, format);
 
+	SET_CONSOLE_TEXT_COLOR_RED();
 	_log_raw("[ERROR] ");
 	_log_vraw(format, list);
 	_log_raw("\n");
+	SET_CONSOLE_TEXT_COLOR_WHITE();
 }
