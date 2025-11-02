9979f99af58220b0afe32be85f8055204fac29d1
WasifBoomz <wasifultragamer@gmail.com>
2025-11-02 17:59:02 -0500

    Increase audio clips and tracks

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -4,8 +4,8 @@
 #include "core/hash.h"
 
 #define AUDIO_3D_SCALE		0.005f
-#define MAX_AUDIO_CLIPS		64
-#define MAX_AUDIO_TRACKS	16
+#define MAX_AUDIO_CLIPS		256
+#define MAX_AUDIO_TRACKS	32
 
 typedef hash audio_clip_h;
 typedef int16_t audio_track_h;
